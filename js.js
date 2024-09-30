b78e4d5b80d20df5bebe6f658661a72668f4069a
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-09-30 23:54:36 +0600

    first

---
+++ js.js
@@ -0,0 +1,41 @@
+console.log('semicolone');
+console.log('my browser');
+console.log('shardha kapoor')
+na = 'tony stark';
+console.log(na);
+
+var age = 25;
+var price = 25;
+var total = age+price;
+console.log(total);
+
+
+var clas = 4;
+console.log(clas)
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
