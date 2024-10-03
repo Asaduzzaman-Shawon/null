6b1d3d3c407b160657ba245d04ecae1104e11ee2
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-10-03 23:11:22 +0600

    second part

---
+++ second.js
@@ -0,0 +1,55 @@
+console.log("hello") 
+
+
+//aritmetic Operator
+
+// let a = 5;
+// let b = 4;
+// console.log('a = ',a , ',   b = ', b);
+// console.log('a + b =' , a+b); 
+// console.log('a ** b =' , a**b);
+// console.log('a++ =', a)
+
+let a = 5;
+//  a++;
+//  a--;
+++a;
+console.log(a);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
